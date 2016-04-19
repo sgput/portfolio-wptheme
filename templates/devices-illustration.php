0e7116133ac91fa93197163f0f9ddb8acfa8b4fe
Spencer Putnam <spencer.putnam@gmail.com>
2016-04-19 11:53:42 -0500

    Remove CONTRIBUTING.md and create new CSS partials

---
+++ templates/devices-illustration.php
@@ -0,0 +1,62 @@
+<!-- 'DEVICES' CSS ILLUSTRATION -->
+<div class="devices center-block">
+  <div class="desktop-wrap">
+    <div class="desktop-monitor">
+      <div class="anim-content">
+        <div class="pg">
+          <ul class="txt">
+            <li></li>
+            <li></li>
+            <li class="thin"></li>
+            <li class="thin"></li>
+            <li class="thin"></li>
+          </ul>
+        </div>
+      </div>
+    </div> <!-- end screen -->
+    <div class="base"></div> <!-- base -->
+  </div> <!--end desktop-wrap-->
+
+
+  <div class="laptop-wrap"> <!-- begin laptop -->
+
+    <div class="laptop-monitor"> <!-- begin screen -->
+      <div class="anim-content">
+        <ul class="txt txt-laptop">
+          <li></li>
+          <li></li>
+          <li class="thin"></li>
+          <li class="thin"></li>
+          <li class="thin"></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="btm"></div> <!-- bottom of laptop -->
+  </div>
+
+  <div class="tablet"> <!-- begin tablet -->
+    <div class="tablet-screen">
+      <div class="tablet-content">
+        <ul class="txt">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="phone"> <!-- begin phone -->
+    <div class="screen">
+      <div class="phone-content">
+        <ul class="txt">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
